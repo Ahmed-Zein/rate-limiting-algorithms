7a85e62471543016a30e7ee1bb5051fcf5a9855b
Ahmed-Zein <ahmedzein3652@gmail.com>
2026-03-13 23:18:42 +0200

    redis initial set up

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	ctx := context.Background()
+
+	val, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Redis connected: ", val)
 }
